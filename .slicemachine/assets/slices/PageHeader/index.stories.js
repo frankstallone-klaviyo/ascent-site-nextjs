e971dee2b8f4b8a5d123c795c32874b26a91a932
Frank Stallone III <frank.stallone@klaviyo.com>
2022-07-28 10:36:10 -0400

    wip: 🚧 Added Prismics dependencies and fired up their slicemachine but struggling to get my figma-plugins page

---
+++ .slicemachine/assets/slices/PageHeader/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/PageHeader';
+
+export default {
+  title: 'slices/PageHeader'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"page_header","items":[],"primary":{"title":[{"type":"heading1","text":"Reinvent 24/365 technologies","spans":[]}],"description":[{"type":"paragraph","text":"Tempor cillum tempor sunt ut do duis enim excepteur.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { linkResolver, repositoryName } from '../prismicio'
 import '../styles/globals.css'
 
-
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, ...props }) => (
+        <Link href={href}>
+          <a {...props} />
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
   )
 }
-
-export default MyApp
