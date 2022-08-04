47e833966016309784705c27de58b3cc33756f5f
Frank Stallone III <frank.stallone@klaviyo.com>
2022-08-04 08:52:32 -0400

    feat: ✨ Updated to next framework, added ts codegen for slices/customtyles

--- slices/RichText/index.js
+++ slices/RichText/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { PrismicRichText } from '@prismicio/react'
 
 const RichText = ({ slice }) => {
-  console.log(slice.primary.wysiwyg)
   return (
   <section>
     <PrismicRichText field={slice.primary.wysiwyg} />
