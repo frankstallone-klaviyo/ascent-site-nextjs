b70c196e50b77a7e992dffd59e30911158bd73f7
Frank Stallone III <frank.stallone@klaviyo.com>
2022-09-09 13:25:05 -0400

    bug: 🐛 next/head required for these favicons

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { linkResolver, repositoryName } from '../prismicio'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -14,6 +15,16 @@ export default function App({ Component, pageProps }) {
         </Link>
       )}
     >
+    <Head>
+      <title>Ascent Design System</title>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+      <meta name="msapplication-TileColor" content="#da532c"/>
+      <meta name="theme-color" content="#ffffff"/>
+    </Head>
       <PrismicPreview repositoryName={repositoryName}>
         <Component {...pageProps} />
       </PrismicPreview>
