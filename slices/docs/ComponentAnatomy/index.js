49c9b0745e4569ad6db45bca28ac35105e054d85
Frank Stallone III <frank.stallone@klaviyo.com>
2022-09-19 13:41:57 -0400

    refactor: ♻️  Fixing key and className issues

--- slices/docs/ComponentAnatomy/index.js
+++ slices/docs/ComponentAnatomy/index.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { PrismicRichText } from '@prismicio/react'
 
 const ComponentAnatomy = ({ slice }) => (
-  <section class="bg-slate-300 rounded-3xl flex flex-row gap-8 p-8 mb-12">
+  <section className="bg-slate-300 rounded-3xl flex flex-row gap-8 p-8 mb-12">
 
-    <ol class="my-0 pl-4 list-decimal">
-      { slice?.items?.map((item, i) => <li class="mb-4">
-          <span class="block font-bold">{ item.part }</span>
+    <ol className="my-0 pl-4 list-decimal">
+      { slice?.items?.map((item, i) => <li className="mb-4" key={i}>
+          <span className="block font-bold">{ item.part }</span>
           <span>{ item.part_description }</span>
       </li>) }
     </ol>    
 
-    <img class="flex-grow my-0" src={slice.primary.anatomy_image.url} alt={slice.primary.anatomy_image.alt} />
+    <img className="flex-grow my-0" src={slice.primary.anatomy_image.url} alt={slice.primary.anatomy_image.alt} />
 
   </section>
 )
--- slices/docs/ComponentGuidelines/index.js
+++ slices/docs/ComponentGuidelines/index.js
@@ -2,22 +2,22 @@ import React from 'react'
 import { PrismicRichText } from '@prismicio/react'
 
 const ComponentGuidelines = ({ slice }) => (
-  <section class="bg-slate-300 rounded-3xl flex flex-col gap-8 p-8 mb-12">
+  <section className="bg-slate-300 rounded-3xl flex flex-col gap-8 p-8 mb-12">
     <PrismicRichText field={slice.primary.title} />
 
     <PrismicRichText field={slice.primary.description} />
 
-    <div class="flex gap-12">
+    <div className="flex gap-12">
       
       { 
         slice?.items?.map((item, i) => 
-          <div class="bg-white flex-1 rounded-2xl overflow-hidden">
+          <div className="bg-white flex-1 rounded-2xl overflow-hidden" key={i}>
             
             <img src={item.image.url} alt={item.image.alt} />
             
-            <div class={ [item.type === "positive" ? "border-green-700 bg-green-400" : "border-red-700 bg-red-400", "border-t-8", "p-4", "flex-grow"].join(" ") }>
-              <p class="font-bold mb-2">{ item.header }</p>
-              <p class="mt-0">{ item.description }</p>
+            <div className={ [item.type === "positive" ? "border-green-700 bg-green-400" : "border-red-700 bg-red-400", "border-t-8", "p-4", "flex-grow"].join(" ") }>
+              <p className="font-bold mb-2">{ item.header }</p>
+              <p className="mt-0">{ item.description }</p>
             </div>
           </div>) 
       }
