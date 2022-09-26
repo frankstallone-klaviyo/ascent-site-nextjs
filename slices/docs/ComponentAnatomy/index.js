072507ada7f88bbe0110ae9d7bb8163f1f344041
Frank Stallone III <frank.stallone@klaviyo.com>
2022-09-26 13:58:34 -0400

    refactor: ♻️  Refining responsive breakpoints for ComponentAnatomy

--- slices/docs/ComponentAnatomy/index.js
+++ slices/docs/ComponentAnatomy/index.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const ComponentAnatomy = ({ slice }) => (
-  <section className="xl:bg-slate-100 xl:rounded-3xl flex flex-col xl:flex-row gap-8 xl:p-8 mb-12">
+  <section className="xl:bg-slate-100 xl:rounded-3xl flex flex-col-reverse 2xl:flex-row gap-8 xl:p-8 mb-12">
+    <ol className="my-0 pl-4 list-decimal grow sm:columns-3 sm:gap-8 2xl:columns-auto">
+      {slice?.items?.map((item, i) => (
+        <li className="mt-0 mb-4 break-inside-avoid-column" key={i}>
+          <span className="block font-bold">{item.part}</span>
+          <span>{item.part_description}</span>
+        </li>
+      ))}
+    </ol>
 
-    <ol className="my-0 pl-4 list-decimal">
-      { slice?.items?.map((item, i) => <li className="mb-4" key={i}>
-          <span className="block font-bold">{ item.part }</span>
-          <span>{ item.part_description }</span>
-      </li>) }
-    </ol>    
-
-    <img className="grow my-0" src={slice.primary.anatomy_image.url} alt={slice.primary.anatomy_image.alt} />
-
+    <img
+      className="grow my-0"
+      src={slice.primary.anatomy_image.url}
+      alt={slice.primary.anatomy_image.alt}
+    />
   </section>
-)
+);
 
-export default ComponentAnatomy
+export default ComponentAnatomy;
