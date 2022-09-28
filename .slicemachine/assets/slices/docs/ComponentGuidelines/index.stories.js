18b18b776cced7dc569dbcafa9251e9de2d7223d
Frank Stallone III <frank.stallone@klaviyo.com>
2022-09-28 10:41:15 -0400

    feat: ✨ Adding Single guideline per row boolean to ComponentGuidelines and changing the flex based on decision

--- .slicemachine/assets/slices/docs/ComponentGuidelines/index.stories.js
+++ .slicemachine/assets/slices/docs/ComponentGuidelines/index.stories.js
@@ -1,9 +1,41 @@
 import MyComponent from '../../../../../slices/docs/ComponentGuidelines';
 
 export default {
-  title: 'slices/docs/ComponentGuidelines'
-}
+  title: 'slices/docs/ComponentGuidelines',
+};
 
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"type":"positive","image":{"dimensions":{"width":900,"height":500},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1494173853739-c21f58b16055"},"header":"slide","description":"round"}],"primary":{"title":"introduced","description":[{"type":"paragraph","text":"Officia duis laboris Lorem amet veniam laborum esse.","spans":[]}]},"slice_type":"component_guidelines","id":"_Default"}} />
-_Default.storyName = ''
+export const _Default = () => (
+  <MyComponent
+    slice={{
+      variation: 'default',
+      version: 'sktwi1xtmkfgx8626',
+      items: [
+        {
+          type: 'positive',
+          image: {
+            dimensions: { width: 900, height: 500 },
+            alt: null,
+            copyright: null,
+            url: 'https://images.unsplash.com/photo-1587840171670-8b850147754e',
+          },
+          header: 'manufacturing',
+          description: 'winter',
+        },
+      ],
+      primary: {
+        title: 'space',
+        description: [
+          {
+            type: 'paragraph',
+            text: 'Esse tempor quis ut est nisi.',
+            spans: [],
+          },
+        ],
+        row: true,
+      },
+      slice_type: 'component_guidelines',
+      id: '_Default',
+    }}
+  />
+);
+_Default.storyName = '';
--- slices/docs/ComponentGuidelines/index.js
+++ slices/docs/ComponentGuidelines/index.js
@@ -4,8 +4,8 @@ import { classNames } from '../../../pages/index';
 import { CheckIcon, NoSymbolIcon } from '@heroicons/react/24/outline';
 
 const ComponentGuidelines = ({ slice }) => {
-  // If there is only one item, resize the container to half width
-  const singleItemClassName = slice.items.length === 1 ? 'w-1/2' : '';
+  // If slice.primary.row is true, guideline container to take entire row
+  const singleGuidelinePerRow = slice.primary.row ? 'flex-col' : 'flex-row';
 
   return (
     <section className='xl:bg-slate-100 xl:rounded-3xl flex flex-col gap-8 xl:p-8 mb-12'>
@@ -13,42 +13,47 @@ const ComponentGuidelines = ({ slice }) => {
 
       <PrismicRichText field={slice.primary.description} />
 
-      <div className={'flex xl:gap-12 gap-8 ' + singleItemClassName}>
-        {slice?.items?.map((item, i) => (
-          <div
-            className='flex flex-col bg-white flex-1 rounded-2xl overflow-hidden'
-            key={i}
-          >
-            <img
-              src={item.image.url}
-              alt={item.image.alt}
-              className='self-center'
-            />
+      <div className={`flex xl:gap-12 gap-8 ${singleGuidelinePerRow}`}>
+        {slice?.items?.map((item, i) => {
+          // If there is only one item & slice.primary.row is false, resize the container to half width
+          const singleItemClassName =
+            slice.items.length === 1 && !slice.primary.row ? 'w-1/2' : 'flex-1';
 
+          return (
             <div
-              className={classNames(
-                item.type === 'positive'
-                  ? 'border-green-700 bg-green-100'
-                  : 'border-red-700 bg-red-100',
-                'border-t-8',
-                'px-6',
-                'pt-6',
-                'pb-8',
-                'lg:pb-12',
-                'grow',
-                'relative'
-              )}
+              className={`flex flex-col bg-white rounded-2xl overflow-hidden ${singleItemClassName}`}
             >
-              {item.type === 'positive' ? (
-                <CheckIcon className='text-green-400 w-6 lg:w-8 absolute bottom-3 right-3' />
-              ) : (
-                <NoSymbolIcon className='text-red-400 w-6 lg:w-8 absolute bottom-3 right-3' />
-              )}
-              <p className='font-bold mb-2 mt-0'>{item.header}</p>
-              <p className='mt-0 mb-1'>{item.description}</p>
+              <img
+                src={item.image.url}
+                alt={item.image.alt}
+                className='self-center'
+              />
+
+              <div
+                className={classNames(
+                  item.type === 'positive'
+                    ? 'border-green-700 bg-green-100'
+                    : 'border-red-700 bg-red-100',
+                  'border-t-8',
+                  'px-6',
+                  'pt-6',
+                  'pb-8',
+                  'lg:pb-12',
+                  'grow',
+                  'relative'
+                )}
+              >
+                {item.type === 'positive' ? (
+                  <CheckIcon className='text-green-400 w-6 lg:w-8 absolute bottom-3 right-3' />
+                ) : (
+                  <NoSymbolIcon className='text-red-400 w-6 lg:w-8 absolute bottom-3 right-3' />
+                )}
+                <p className='font-bold mb-2 mt-0'>{item.header}</p>
+                <p className='mt-0 mb-1'>{item.description}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
